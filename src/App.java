b680769f9d3cd523931248a526392eb76c3a3f73
Javier Eduardo <javiereduardo55xd@gmail.com>
2023-03-08 20:35:49 -0700

    Interfaz lista

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 public class App extends JFrame {
     private JTextArea txt;
     private JButton escanear;
+    private JPanel listado;
 
     public static void main(String[] args) throws Exception {
         new App();
@@ -16,11 +17,23 @@ public class App extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLayout(null);
+        JLabel label = new JLabel("Listado de tokens");
+        listado = new JPanel();
         txt = new JTextArea();
-        txt.setBounds(20, 50, 380, 400);
+        escanear = new JButton("Escanear");
+        {
+            escanear.setBounds(160, 480, 120, 40);
+            label.setBounds(400, 25, 200, 10);
+            listado.setBounds(400, 50, 380, 400);
+            txt.setBounds(20, 50, 380, 400);
+        }
         txt.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        txt.setFont(new Font("Consola", 10, 16));
+        txt.setFont(new Font("Consola", 10, 15));
+        listado.setLayout(new FlowLayout(1));
         add(txt);
+        add(escanear);
+        add(label);
+        add(listado);
         setVisible(true);
     }
 }
