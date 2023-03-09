7091ddab7572ef9fccd6a31035e56b26021400fa
Javier Eduardo <javiereduardo55xd@gmail.com>
2023-03-08 21:13:08 -0700

    poniendo comportamineto

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class App extends JFrame {
+public class App extends JFrame implements ActionListener {
     private JTextArea txt;
     private JButton escanear;
     private JPanel listado;
@@ -12,12 +12,13 @@ public class App extends JFrame {
     }
 
     public App() {
+        Font fuente = new Font("Tahoma", 15, 15);
         setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLayout(null);
-        JLabel label = new JLabel("Listado de tokens");
+        JLabel label = new JLabel("Listado de Tokens");
         listado = new JPanel();
         txt = new JTextArea();
         escanear = new JButton("Escanear");
@@ -28,12 +29,24 @@ public class App extends JFrame {
             txt.setBounds(20, 50, 380, 400);
         }
         txt.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        txt.setFont(new Font("Consola", 10, 15));
-        listado.setLayout(new FlowLayout(1));
+        txt.setFont(fuente);
+        listado.setLayout(new BoxLayout(listado, 1));
+        escanear.addActionListener(this);
         add(txt);
         add(escanear);
         add(label);
         add(listado);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == escanear) {
+            String[] lineas = txt.getText().split("\\r?\\n");
+            for (String linea : lineas) {
+                listado.add(new JLabel(linea));
+            }
+            revalidate();
+        }
     }
 }
