a95d8cc5691a6faa56c41765b694105a88030adf
Javier Eduardo <javiereduardo55xd@gmail.com>
2023-03-11 15:08:39 -0700

    arreglando listado de tokens

--- src/App.java
+++ src/App.java
@@ -9,13 +9,13 @@ public class App extends JFrame implements ActionListener {
     private String alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private String numeros = "0123456789";
     private String rels = "><!=";
+    private Font fuente = new Font("Tahoma", 15, 15);
 
     public static void main(String[] args) throws Exception {
         new App();
     }
 
     public App() {
-        Font fuente = new Font("Tahoma", 15, 15);
         setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -25,26 +25,29 @@ public class App extends JFrame implements ActionListener {
         listado = new JPanel();
         txt = new JTextArea();
         escanear = new JButton("Escanear");
+        JScrollPane scrollListado = new JScrollPane(listado);
         {
             escanear.setBounds(160, 480, 120, 40);
             label.setBounds(400, 25, 200, 10);
-            listado.setBounds(400, 50, 380, 400);
+            scrollListado.setBounds(400, 50, 380, 400);
             txt.setBounds(20, 50, 380, 400);
         }
         txt.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         txt.setFont(fuente);
         listado.setLayout(new GridLayout(0, 2));
+        listado.setAlignmentY(Component.TOP_ALIGNMENT);
         escanear.addActionListener(this);
         add(txt);
         add(escanear);
         add(label);
-        add(listado);
+        add(scrollListado);
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == escanear) {
+            listado.removeAll();
             String[] lineas = txt.getText().split("\\r?\\n");
             for (String linea : lineas)
                 validarToken(linea);
@@ -76,11 +79,20 @@ public class App extends JFrame implements ActionListener {
             if (numeros.contains("" + actual))
                 numero += actual;
         }
-        listado.add(new JLabel(Id));
-        listado.add(new JLabel("ID"));
-        listado.add(new JLabel(OpRel));
-        listado.add(new JLabel("Operador relacional"));
-        listado.add(new JLabel(numero));
-        listado.add(new JLabel("numero"));
+        JLabel[] labels = {
+                new JLabel(Id),
+                new JLabel("ID"),
+                new JLabel(OpRel),
+                new JLabel("Operador relacional"),
+                new JLabel(numero),
+                new JLabel("numero")
+        };
+        for (JLabel label : labels) {
+            label.setMinimumSize(new Dimension(100, 25));
+            label.setMaximumSize(new Dimension(100, 25));
+            label.setPreferredSize(new Dimension(100, 25));
+            label.setFont(fuente);
+            listado.add(label);
+        }
     }
 }
